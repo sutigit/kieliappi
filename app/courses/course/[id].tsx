1e687ba728c80aa615f7c5995ef4535077d99817
sutigit <panyanilben@gmail.com>
2025-04-28 15:29:59 +0300

    ansewr banners init

--- app/courses/course/[id].tsx
+++ app/courses/course/[id].tsx
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native";
 import { Link, useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CourseHeader from "@/app/components/course-header";
+import CourseHeader from "@/app/courses/components/course-header";
 
 export default function Course() {
   const { id } = useLocalSearchParams();
