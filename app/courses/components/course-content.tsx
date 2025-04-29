9aa2a94bf34cbbf3436fe45a659aef984940a8d8
sutigit <panyanilben@gmail.com>
2025-04-29 20:31:54 +0300

    yea shit works

--- app/courses/components/course-content.tsx
+++ app/courses/components/course-content.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ScrollView, View, Text } from "react-native";
 import Button from "@/app/components/button";
 import Feedback from "../components/feedback";
+import Frame from "./frame";
 
 interface Frame {
   name: number;
@@ -21,7 +22,7 @@ export default function CourseContent({
   progress: number;
   setProgress: (progress: number) => void;
 }) {
-  const [containerHeight, setContainerHeight] = useState(500);
+  const [containerHeight, setContainerHeight] = useState(0);
   const [frames, setFrames] = useState<Frame[]>([]);
 
   useEffect(() => {
@@ -46,42 +47,16 @@ export default function CourseContent({
         }}
       >
         {frames.map((frame, index) => (
-          <View
+          <Frame
             key={index}
-            style={{
-              height:
-                index === frames.length - 1 ? containerHeight : frame.height,
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  backgroundColor: frame.bg,
-                  height: frame.height,
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Text style={{ fontSize: 24 }}>{`Frame ${frame.name}`}</Text>
-              </View>
-            </View>
-
-            {index === frames.length - 1 && (
-              <View style={{ padding: 30 }}>
-                <Button
-                  title="Jatka"
-                  onPress={() => setProgress(progress + 1)}
-                />
-              </View>
-            )}
-          </View>
+            content={frame}
+            isLast={index === frames.length - 1}
+            progress={progress}
+            setProgress={setProgress}
+            containerHeight={containerHeight}
+          />
         ))}
       </ScrollView>
-      <Feedback answer={null} xp={10} />
     </View>
   );
 }
--- app/courses/components/frame.tsx
+++ app/courses/components/frame.tsx
@@ -1,5 +1,48 @@
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import Button from "@/app/components/button";
+import Feedback from "./feedback";
 
-export default function Frame({ content }: { content: any }) {
-  return <View style={{}}></View>;
+export default function Frame({
+  content,
+  isLast,
+  progress,
+  setProgress,
+  containerHeight,
+}: {
+  content: any;
+  isLast: boolean;
+  progress: number;
+  setProgress: (progress: number) => void;
+  containerHeight: number;
+}) {
+  const [answer, setAnswer] = useState<"correct" | "wrong" | null>(null);
+
+  return (
+    <View
+      style={{
+        height: isLast ? containerHeight : content.height,
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            backgroundColor: content.bg,
+            height: content.height,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 24 }}>{`content ${content.name}`}</Text>
+        </View>
+      </View>
+
+      {isLast && (
+        <View style={{ padding: 30, position: "relative" }}>
+          <Button title="Jatka" onPress={() => setProgress(progress + 1)} />
+          <Feedback answer={answer} xp={10} />
+        </View>
+      )}
+    </View>
+  );
 }
